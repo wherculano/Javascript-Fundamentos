68d1be392fe0c734ef01fcd32e4ddb65fa0ab938
wagner <wagner.santos@accurate.com.br>
2023-02-13 09:05:45 -0300

    Strings

---
+++ type_string.js
@@ -0,0 +1,27 @@
+const meuNome = "Goku"
+const frase = "Oi, eu sou o "
+// concatenacao de strings
+console.log(frase + meuNome)
+
+// funcoes de formatacao
+const textoEmMinusculo = "Hello WORLD!"
+console.log(textoEmMinusculo.toLowerCase())
+
+const textoEmMMaiusculo = "hello javaScript!"
+console.log(textoEmMMaiusculo.toUpperCase())
+
+// tamanho da string
+const senha = "m1Nh453Nh43h53gUr4"
+console.log("Minha senha possui " + senha.length + " caracteres")
+
+// substituindo caracteres
+console.log("I'm studying JavaLanguage".replace("Language", "Script"))
+
+// Template String
+console.log(`Oi, eu sou o ${meuNome}!!`)
+
+
+/*
+Mais metodos em:
+https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String#compatibilidade_com_navegadores
+*/
