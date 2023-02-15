5856f5fa86d132cdd40693dc938dbf8c8d26132d
wagner <wagner.santos@accurate.com.br>
2023-02-15 15:51:49 -0300

    Operadores (logicos e matematicos)

---
+++ aula_008_operadores.js
@@ -0,0 +1,73 @@
+// soma
+console.log(`10 + 9 = ${10 + 9}`)
+
+// subtracao
+console.log(`20 - 1 = ${20 - 1}`)
+
+// divisao
+console.log(`57 / 3 = ${57 / 3}`)
+
+// multiplicacao
+console.log(`9.5 * 2 = ${9.5 * 2}`)
+
+// Módulo (resto da divisão)
+console.log(`10 % 2 => ${10 % 2}`)
+
+// Exponenciação
+console.log(`2 ** 3 = ${2**3}`)
+
+console.log('-=-=-=-=-=--=-=-=-=-=')
+
+// Incremento
+let num1 = 1
+console.log("num1: " + num1)
+console.log(`num1++ => ${num1++}`)
+console.log("num1: " + num1)
+console.log('---------------')
+
+let num2 = 9
+console.log("num2: " + num2)
+console.log(`++num2 => ${++num2}`)
+console.log("num2: " + num2)
+console.log('---------------')
+
+// Decremento
+let num3 = 3
+console.log("num3: " + num3)
+console.log(`num3-- => ${num3--}`)
+console.log("num3: " + num1)
+console.log('---------------')
+
+let num4 = 9
+console.log("num4: " + num4)
+console.log(`--num4 => ${--num4}`)
+console.log("num4: " + num4)
+
+console.log('-=-=-=-=-=--=-=-=-=-=')
+
+// Ou -> apenas um sendo true ja retorna true como resultado
+console.log(`true || false => ${true || false}`)
+
+// E -> apenas um sendo false ja retorna false como resultado
+console.log(`true && false => ${true && false}`)
+
+// Diferente
+console.log(`5 != "5" => ${5 != "5"}`)
+
+// Extritamente diferente
+console.log(`5 !== "5" => ${5 !== "5"}`)
+
+// Igual
+console.log(`5 == "5": ${5 == "5"}`)
+
+// Extritamente igual
+console.log(`5 === "5": ${5 === "5"}`)
+
+// Negacao
+console.log(`!false => ${!false}`)
+
+console.log('-=-=-=-=-=--=-=-=-=-=')
+
+/* Mais exemplo:
+https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Expressions_and_operators
+*/
